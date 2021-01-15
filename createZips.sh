5f3b43d2b829c5cd48f6b5d73b671dcf0a33fe42
anbcodes <anbcodes@protonmail.com>
2021-01-15 10:09:52 -0500

    added .gitignore and created a script to generate the zip files

---
+++ createZips.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+workingdir=`pwd`
+foldername=`basename $workingdir`
+
+exclude="'.git/**' '.git/' 'createZips.sh' '*.zip' '.gitignore'"
+
+excludeFirefox=
+excludeChrome=
+for x in $exclude; do
+    excludeFirefox="$excludeFirefox -x $x"
+    excludeChrome="$excludeChrome -x \"$foldername/${x:1:-1}\""
+done
+
+excludeFirefox=${excludeFirefox:1}
+excludeChrome=${excludeChrome:1}
+
+firefoxZipCommand="zip $excludeFirefox -r HTMW-ad-removal-firefox.zip ."
+eval "$firefoxZipCommand"
+
+pushd ..
+    chromeZipCommand="zip $excludeChrome -r HTMW-ad-removal-chrome.zip $foldername"
+    eval "$chromeZipCommand"
+popd
