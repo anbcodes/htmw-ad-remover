77eca09cf323720cd3187405040100afa9fce29d
anbcodes <anbcodes@protonmail.com>
2021-01-14 21:12:40 -0500

    removes more ads

--- removeads.js
+++ removeads.js
@@ -1,1 +1,10 @@
-document.querySelectorAll(".pa-global-class, .ad-panel, #adblocker-notice").forEach(node => node.style.display = 'none')
+document.querySelectorAll(".pa-global-class, .ad-panel, #adblocker-notice, .icADSlot, [id^='ad_is']").forEach(function (node) {
+    node.style.display = 'none'
+});
+
+setInterval(function () {
+    document.querySelectorAll(".pa-global-class, .ad-panel, #adblocker-notice, .icADSlot, [id^='ad_is']")
+    .forEach(function (node) {
+            node.style.display = 'none'
+    })
+}, 1000);
